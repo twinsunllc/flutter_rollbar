1f91eb343fd703ce3391a0f27de345143dffcb7d
Jami Couch <jami.couch@twinsunsolutions.com>
2019-03-29 15:36:44 -0500

    Resolve publish errors

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <device_info/DeviceInfoPlugin.h>
+#import <package_info/PackageInfoPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
+  [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
+}
+
+@end
